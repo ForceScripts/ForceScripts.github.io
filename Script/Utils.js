5c648192d772dbebc198f0bf6c4400d9d9e18f90
ForceScripts <71199348+IDKFORCE@users.noreply.github.com>
2022-09-06 14:23:01 -0700

    Add files via upload

---
+++ Script/Utils.js
@@ -0,0 +1,64 @@
+let Typewriter = 0;
+const txt = "ForceScripts";
+const txts = "Social";
+
+function applyCursorRippleEffect(t) {
+    const e = document.createElement("div");
+    e.className = "ripple", document.body.appendChild(e), e.style.left = t.clientX + "px", e.style.top = t.clientY + "px", e.style.animation = "ripple-effect .4s  linear", e.onanimationend = () => {
+        document.body.removeChild(e)
+    }
+}
+
+document.onclick = () => applyCursorRippleEffect(event);
+try {
+    const c = document.body.getElementsByTagName("*");
+    for (let t of c) t.onclick = t => t.stopPropagationw()
+} catch (t) {
+    console.log(t)
+}
+
+function typeWriter() {
+    const words = ["ForceScripts", "ForceScripts"];
+    let i = 0;
+    let offset = 0;
+    let forwards = true;
+    let skip_count = 0;
+    setInterval(() => {
+        if (forwards) {
+            if (offset >= words[i].length) {
+                skip_count++;
+                if (skip_count === 20) {
+                    forwards = false;
+                    skip_count = 0;
+                }
+            }
+        } else if (offset === 0) {
+            forwards = true;
+            i++;
+            offset = 0;
+            if (i >= words.length) {
+                i = 0;
+            }
+        }
+        if (skip_count === 0) {
+            if (forwards) {
+                offset++;
+            } else {
+                offset--;
+            }
+        }
+        const text = words[i].substring(0, offset) + "!";
+        document.title = text
+        document.getElementById("Main-Text").innerHTML = text;
+    }, 80);
+}
+
+function fetchgithubdata() {
+    setTimeout(() => {
+        fetch("https://api.github.com/users/IDKFORCE").then(t => {
+            t.json().then(t => {
+                document.getElementById("count").innerHTML = `My Github Stats <br>Followers: ${t.followers}<br>Following: ${t.following}<br>Public Repos: ${t.public_repos}`
+            });
+        });
+    }, 100);
+}
